54c43209c358a930f91c72782f5b977034b1cf8e
Rayfess <153087368+Rayfess@users.noreply.github.com>
2025-11-15 15:46:05 +0700

    feat: adding sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,59 @@
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 fixed py-2 z-50 shadow-sm">
-      <div className="navbar-start">
-        {/* dropdown < 1024 */}
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+    <div className="drawer drawer-end">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+        <div className="navbar bg-base-100 fixed py-2 z-50 shadow-sm w-full">
+          <div className="mx-2 flex-1 px-2">
+            <a className="btn btn-ghost text-primary font-bold text-3xl">
+              LearnBy DOING
+            </a>
           </div>
-          <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-primary-500 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
+          <div className="hidden flex-none lg:flex items-center">
+            <ul className="menu menu-horizontal space-x-5 px-5">
+              <li>
+                <a className="text-lg">Services</a>
+              </li>
+              <li>
+                <a className="text-lg">Project</a>
+              </li>
+              <li>
+                <a className="text-lg">Contact Us</a>
+              </li>
+            </ul>
+            <a className="btn btn-primary text-lg lg:me-5">Get Started</a>
+          </div>
+          <div className="flex-none lg:hidden">
+            <label
+              htmlFor="my-drawer-2"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-6 w-6 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-2"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-base-200 min-h-full w-80 p-4">
+          <div className="py-20 space-y-5">
             <li>
               <a className="text-lg">Services</a>
             </li>
@@ -34,26 +63,9 @@ const Navbar = () => {
             <li>
               <a className="text-lg">Contact Us</a>
             </li>
-          </ul>
-        </div>
-        {/* dropdown < 1024 */}
-        <a className="btn btn-ghost text-primary font-bold text-3xl">
-          LearnBy DOING
-        </a>
-      </div>
-      <div className="navbar-end lg:mx-5 hidden lg:flex">
-        <ul className="menu menu-horizontal space-x-5 px-5">
-          <li>
-            <a className="text-lg">Services</a>
-          </li>
-          <li>
-            <a className="text-lg">Project</a>
-          </li>
-          <li>
-            <a className="text-lg">Contact Us</a>
-          </li>
+            <a className="btn btn-primary text-lg">Get Started</a>
+          </div>
         </ul>
-        <a className="btn btn-primary text-lg">Button</a>
       </div>
     </div>
   );
